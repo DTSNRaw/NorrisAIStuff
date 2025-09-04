126bb06c024ebe5647cb32fc8109ef62c7f9c555
DTSNRaw <dotsond3455@student.faytechcc.edu>
2025-09-04 15:54:15 +0000

    Finished

--- mt1.cpp
+++ mt1.cpp
@@ -4,10 +4,14 @@ M1T1 for online sections
 Damian Dotson, also have you for cts 285
 9/4/25
 the computer will say hello
+Simple program. 
 */
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(){
+
+    cout << "Hello, user!" << endl;
+
     return 0;
 }
