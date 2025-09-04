e096809fced80e5a78bf74b89a244696bc52c077
DTSNRaw <dotsond3455@student.faytechcc.edu>
2025-09-04 15:38:54 +0000

    m1t1Cpluss

---
+++ mt1.cpp
@@ -0,0 +1,13 @@
+/*
+CSC 134
+M1T1 for online sections
+Damian Dotson, also have you for cts 285
+9/4/25
+the computer will say hello
+*/
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
