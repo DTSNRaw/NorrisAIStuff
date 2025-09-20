4d1204e6b8045d173c0f87e046d017a6e6a0e8a4
DTSNRaw <dotsond3455@student.faytechcc.edu>
2025-09-20 18:47:05 +0000

    COMPLETE

--- M2T2.cpp
+++ M2T2.cpp
@@ -27,10 +27,13 @@ int main() {
    total = item_price + tax_amount;
 
    cout << setprecision(2) << fixed;
-   cout << total << endl;
-
-
-
+   cout << "Thank you for sacrificing your money to us!" << endl;
+   cout << "---------------------------" << endl;
+   cout << item << "\t$" << item_price << endl;
+   cout << "Tax" << "\t\t$" << tax_amount << endl;
+   cout << "---------------------------" << endl;
+    cout << "Total" << "\t\t$" << total << endl;
+    cout << endl;
 
 
 
