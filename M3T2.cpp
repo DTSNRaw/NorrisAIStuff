9a8268e7d728f23b807f726daf44ef1e65dadc31
DTSNRaw <dotsond3455@student.faytechcc.edu>
2025-09-28 22:27:59 +0000

    M3T2

---
+++ M3T2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // declare the variables
+    int widthOne, lengthOne, widthTwo, lengthTwo;
+    int areaOne, areaTwo;
+    // ask for user input
+    cout << "Please enter the width and height of the first" << endl;
+    cout << "rectangle, separated by a space or newline" << endl;
+    cin >> widthOne >> lengthOne;
+
+    cout << "Please enter the width and height of the Second" << endl;
+    cout << "rectangle, separated by a space or newline" << endl;
+    cin >> widthTwo >> lengthTwo;
+
+    // calculate answer
+    areaOne = widthOne * lengthOne;
+    areaTwo = widthTwo * lengthTwo;
+
+    // display results
+    cout << "rectangle one has area of " << areaOne << endl;
+    cout << "rectangle two has area of " << areaTwo << endl;
+    
+    // determine which is bigger
+   
+    if (areaOne > areaTwo) {
+        cout << "Rectangle one is bigger" << endl;
+    }
+
+    else if (areaTwo > areaOne) {
+        cout << "rectangle two is bigger" << endl;
+    }
+
+     else if (areaOne == areaTwo) {
+        cout << "They are the same size" << endl;
+    }
+}
